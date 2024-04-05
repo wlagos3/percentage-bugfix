2a1b149e730acc9ce66f170d4f39498d0f7ca125
wlagos3 <wlagos3@outlook.com>
2024-04-05 03:47:21 -0400

    revert

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include "Geode/binding/PlayLayer.hpp"
 #include "Geode/cocos/label_nodes/CCLabelBMFont.h"
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
@@ -9,17 +8,6 @@ class $modify(PlayLayer) {
     CCLabelBMFont* percentLabel;
 	CCSprite* progressSprite;
 	bool useOldLogic = false;
-    float getCurrentPercent(){
-         if (m_player1->getPositionX() > 0 && PlayLayer::getCurrentPercent() == 0 && m_fields->useOldLogic == false){
-                m_fields->useOldLogic = true;
-        }
-        if (m_fields->useOldLogic){
-            return (m_player1->getPositionX() / m_endPortal->getPositionX()) * 100.0;
-        }
-        else {
-            return PlayLayer::getCurrentPercent();
-        }
-    }
     void updateProgressbar() {
         PlayLayer::updateProgressbar();
         if (!m_level->isPlatformer()){
@@ -38,9 +26,14 @@ class $modify(PlayLayer) {
                 m_fields->progressSprite = typeinfo_cast<CCSprite*>(m_progressBar->getChildren()->objectAtIndex(0));
         } else {
             float percent;			
-            percent = (m_player1->getPositionX() / m_endPortal->getPositionX()) * 100.0;
-            m_fields->percentLabel->setString(fmt::to_string((int) percent).append("%").c_str());
-            m_fields->progressSprite->setTextureRect(CCRect(0,0,(m_progressBar->getTextureRect().getMaxX() - 5) * (percent / 100), m_progressBar->getTextureRect().getMaxY() / 2));
+            if (m_player1->getPositionX() > 0 && getCurrentPercent() == 0 && m_fields->useOldLogic == false){
+                m_fields->useOldLogic = true;
+            }
+            if(m_fields->useOldLogic){
+                percent = (m_player1->getPositionX() / m_endPortal->getPositionX()) * 100.0;
+                m_fields->percentLabel->setString(fmt::to_string((int) percent).append("%").c_str());
+                m_fields->progressSprite->setTextureRect(CCRect(0,0,(m_progressBar->getTextureRect().getMaxX() - 5) * (percent / 100), m_progressBar->getTextureRect().getMaxY() / 2));
+                }
             }
         }
     }
