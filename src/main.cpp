21a2b06a390c5bfc9132d10ece69010a2af8d768
wlagos3 <wlagos3@outlook.com>
2024-04-05 03:35:05 -0400

    update logic for compatibility

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include "Geode/binding/PlayLayer.hpp"
+#include "Geode/cocos/label_nodes/CCLabelBMFont.h"
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
 #include <fmt/format.h>
@@ -7,19 +9,27 @@ class $modify(PlayLayer) {
     CCLabelBMFont* percentLabel;
 	CCSprite* progressSprite;
 	bool useOldLogic = false;
+    float getCurrentPercent(){
+         if (m_player1->getPositionX() > 0 && PlayLayer::getCurrentPercent() == 0 && m_fields->useOldLogic == false){
+                m_fields->useOldLogic = true;
+        }
+        if (m_fields->useOldLogic){
+            return (m_player1->getPositionX() / m_endPortal->getPositionX()) * 100.0;
+        }
+        else {
+            return PlayLayer::getCurrentPercent();
+        }
+    }
     void updateProgressbar() {
         PlayLayer::updateProgressbar();
         if (!m_level->isPlatformer()){
 		//Credit to FireMario211
 		//https://github.com/FireMario211/Golden-Best/blob/fae9033b781e00ecb86557426fad8e68d19b4ba5/src/main.cpp#L10-L24
             if (m_fields->percentLabel == nullptr) {
-                for (size_t i = 0; i < this->getChildrenCount(); i++) {
-                    auto obj = this->getChildren()->objectAtIndex(i);
-                    if (typeinfo_cast<CCLabelBMFont*>(obj) && m_fields->percentLabel == nullptr) {
-                        auto labelTest = static_cast<CCLabelBMFont*>(obj);
-                        auto labelEnding = labelTest->getString() + (strlen(labelTest->getString()) - 1);
-                        if (strcmp(labelEnding, "%") == 0) {
-                            m_fields->percentLabel = labelTest;;
+                for (int i = 0; i < this->getChildrenCount(); i++) {
+                    if (CCLabelBMFont* object = typeinfo_cast<CCLabelBMFont*>(this->getChildren()->objectAtIndex(i))) {
+                        if (strcmp(&object->getString()[(strlen(object->getString()) - 1)], "%") == 0) {
+                            m_fields->percentLabel = object;;
                             break;
                         }
                     }
@@ -28,14 +38,9 @@ class $modify(PlayLayer) {
                 m_fields->progressSprite = typeinfo_cast<CCSprite*>(m_progressBar->getChildren()->objectAtIndex(0));
         } else {
             float percent;			
-            if (m_player1->getPositionX() > 0 && getCurrentPercent() == 0 && m_fields->useOldLogic == false){
-                m_fields->useOldLogic = true;
-            }
-            if(m_fields->useOldLogic){
-                percent = (m_player1->getPositionX() / m_endPortal->getPositionX()) * 100.0;
-                m_fields->percentLabel->setString(fmt::to_string((int) percent).append("%").c_str());
-                m_fields->progressSprite->setTextureRect(CCRect(0,0,(m_progressBar->getTextureRect().getMaxX() - 5) * (percent / 100), m_progressBar->getTextureRect().getMaxY() / 2));
-                }
+            percent = (m_player1->getPositionX() / m_endPortal->getPositionX()) * 100.0;
+            m_fields->percentLabel->setString(fmt::to_string((int) percent).append("%").c_str());
+            m_fields->progressSprite->setTextureRect(CCRect(0,0,(m_progressBar->getTextureRect().getMaxX() - 5) * (percent / 100), m_progressBar->getTextureRect().getMaxY() / 2));
             }
         }
     }
