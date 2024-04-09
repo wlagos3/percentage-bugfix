37f9712660fbb2d20b7507504a5c41f2edcfba6b
wlagos3 <wlagos3@outlook.com>
2024-04-09 02:13:58 -0400

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
 
-
-#include "Geode/cocos/sprite_nodes/CCSprite.h"
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
 #include <Geode/modify/GJBaseGameLayer.hpp>
@@ -12,7 +10,6 @@
 using namespace geode::prelude;
 
 class $modify(PlayLayer) {
-    
     CCLabelBMFont* percentLabel;
 	CCSprite* progressSprite;
 	bool useOldLogic = false;
@@ -34,10 +31,10 @@ class $modify(PlayLayer) {
                 m_fields->progressSprite = progressSprite;
                 static_cast<CCNode*>(progressSprite)->setID("level-progress-bar-sprite");
         } else {	
-            if (m_player1->getPositionX() > 0 && getCurrentPercent() == 0 && m_fields->useOldLogic == false){
+            if (!Mod::get()->getSettingValue<bool>("force-enable") && m_player1->getPositionX() > 0 && getCurrentPercent() == 0 && m_fields->useOldLogic == false){
                 m_fields->useOldLogic = true;
             }
-            if(m_fields->useOldLogic || Mod::get()->getSettingValue<bool>("force-enable")){
+            if (Mod::get()->getSettingValue<bool>("force-enable") || (m_fields->useOldLogic)){
                 float percent = (m_player1->getPositionX() / m_endPortal->getPositionX()) * 100.0;
                 std::ostringstream s;
                 s << std::fixed;
